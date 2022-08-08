a3ef34df4c5a91fb3af9e115bbb740b3a2d96975
Hiroshi Atsuta <atsuta@ieee.org>
2022-08-08 13:44:58 +0900

    Fix scripts

--- scripts/extract_images_track_periodic.sh
+++ scripts/extract_images_track_periodic.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-TARGET_DIR="$HOME/Dropbox/develop/data/20220719/predict_ctrl_input_mlp"
-FILENAME_BASE="pressure_at_valve"
+TARGET_DIR="$HOME/Dropbox/develop/data/20220808/track_periodic_traj_esn"
+FILENAME_BASE="q"
 
 mkdir -p "${TARGET_DIR}/${FILENAME_BASE}"
 for dir in "$TARGET_DIR/"*; do
   if [[ -d "$dir" ]]; then
     dirname=$(basename -- "$dir")
     if [[ "$dirname" != "$FILENAME_BASE" ]]; then
-      cp "${dir}/${FILENAME_BASE}"*.png "${TARGET_DIR}/${FILENAME_BASE}/${dirname}.png"
+       cp "${dir}/${FILENAME_BASE}"*.png "${TARGET_DIR}/${FILENAME_BASE}/${dirname}.png"
     fi
   fi
 done
--- scripts/track_periodic_traj_mlp_20220719.sh
+++ scripts/track_periodic_traj_mlp_20220719.sh
@@ -6,7 +6,7 @@ NOW=$(date +"%Y%m%d_%H%M%S")
 TRAIN_DATA_SET_A=("$HOME/Dropbox/develop/data/20220712/data_joints-2-3-4-5")
 TRAIN_DATA_SET_B=("${TRAIN_DATA_SET_A[@]}" "$HOME/Dropbox/develop/data/20220719/data_joints-2-3-4-5_time-1-2")
 TRAIN_DATA_SET_C=("${TRAIN_DATA_SET_B[@]}" "$HOME/Dropbox/develop/data/20220719/data_joints-2-3-4-5_time-2-4")
-OUTPUT_BASE="$HOME/Dropbox/develop/data/20220720/track_periodic_traj_mlp"
+OUTPUT_BASE="$HOME/Dropbox/develop/data/20220808/track_periodic_traj_mlp"
 
 JOINT=(5)
 # N_PREDICT=(1 3 5 7 10 15 20 30)
@@ -14,7 +14,6 @@ N_PREDICT=(10)
 SCALER=("minmax")
 DURATION=30
 
-DATASET=("A" "B" "C")
 TRAJECTORY=("sin" "step")
 AMPLITUDE=(10 20 30 40)
 PERIOD=(1 3 5 10)
@@ -24,10 +23,9 @@ LOG="${OUTPUT_BASE}/${NOW}.log"
 mkdir -p "$OUTPUT_BASE"
 :> "$LOG"
 
-# TRAIN DATA SET A
-if [[ " ${DATASET[*]} " =~ " A " ]]; then
-  output="${OUTPUT_BASE}"/SET-A_joint-5_n-predict-10_scaler-minmax_sin_amp-10_period-1.csv
-  cmd=(pdm run ./apps/track_periodic_traj_mlp.py --train-data "${TRAIN_DATA_SET_A[@]}" --joint 5 --n-predict 10 --n-ctrl-period 1 --scaler minmax --output "${output}" --trajectory sin --time-duration "$DURATION" --amplitude 10 --period 1 --ctrl mlp)
+function do_all(){
+  dataset=$(echo "$1" | tr '[:lower:]' '[:upper:]'); shift 1
+  cmd=(pdm run ./apps/track_periodic_traj_mlp.py --train-data "$@" --joint 5 --output "data.csv" --time-duration 10 --n-predict 10 --n-ctrl-period 1 --ctrl mlp)
   echo "${cmd[@]}"
   "${cmd[@]}"
   for j in "${JOINT[@]}"; do
@@ -36,9 +34,9 @@ if [[ " ${DATASET[*]} " =~ " A " ]]; then
         for t in "${TRAJECTORY[@]}"; do
           for a in "${AMPLITUDE[@]}"; do
             for p in "${PERIOD[@]}"; do
-              echo "TRAIN SET: A, joint: $j, n_predict: $k, scaler: $s, traj: $t, A:$a, T:$p"
-              output="${OUTPUT_BASE}"/SET-A_joint-"$j"_n-predict-"$k"_scaler-"$s"_"$t"_amp-"$a"_period-"$p".csv
-              cmd=(pdm run ./apps/track_periodic_traj_mlp.py --train-data "${TRAIN_DATA_SET_A[@]}" --joint "${j}" --n-predict "${k}" --n-ctrl-period 1 --scaler "${s}" --output "${output}" --trajectory "${t}" --time-duration "$DURATION" --amplitude "${a}" --period "${p}" --ctrl mlp --model "model.joblib")
+              echo "SET: ${dataset}, joint: $j, n_predict: $k, scaler: $s, traj: $t, A:$a, T:$p"
+              output="${OUTPUT_BASE}"/SET-"${dataset}"_joint-"$j"_n-predict-"$k"_scaler-"$s"_"$t"_amp-"$a"_period-"$p".csv
+              cmd=(pdm run ./apps/track_periodic_traj_mlp.py --train-data "$@" --joint "${j}" --n-predict "${k}" --n-ctrl-period 1 --scaler "${s}" --output "${output}" --trajectory "${t}" --time-duration "$DURATION" --amplitude "${a}" --period "${p}" --ctrl mlp --model "model.joblib")
               echo "${cmd[@]}" >>"$LOG"
               "${cmd[@]}"
               cmd=(python ./apps/scripts/plot_track_traj.py "$output" --joint 5 -d "${output%.*}" -s -x)
@@ -50,59 +48,8 @@ if [[ " ${DATASET[*]} " =~ " A " ]]; then
       done
     done
   done
-fi
+}
 
-# TRAIN DATA SET B
-if [[ " ${DATASET[*]} " =~ " B " ]]; then
-  output="${OUTPUT_BASE}"/SET-B_joint-5_n-predict-10_scaler-minmax_sin_amp-10_period-1.csv
-  cmd=(pdm run ./apps/track_periodic_traj_mlp.py --train-data "${TRAIN_DATA_SET_B[@]}" --joint 5 --n-predict 10 --n-ctrl-period 1 --scaler minmax --output "${output}" --trajectory sin --time-duration "$DURATION" --amplitude 10 --period 1 --ctrl mlp)
-  echo "${cmd[@]}"
-  "${cmd[@]}"
-  for j in "${JOINT[@]}"; do
-    for k in "${N_PREDICT[@]}"; do
-      for s in "${SCALER[@]}"; do
-        for t in "${TRAJECTORY[@]}"; do
-          for a in "${AMPLITUDE[@]}"; do
-            for p in "${PERIOD[@]}"; do
-              echo "TRAIN SET: B, joint: $j, n_predict: $k, scaler: $s, traj: $t, A:$a, T:$p"
-              output="${OUTPUT_BASE}"/SET-B_joint-"$j"_n-predict-"$k"_scaler-"$s"_"$t"_amp-"$a"_period-"$p".csv
-              cmd=(pdm run ./apps/track_periodic_traj_mlp.py --train-data "${TRAIN_DATA_SET_B[@]}" --joint "${j}" --n-predict "${k}" --n-ctrl-period 1 --scaler "${s}" --output "${output}" --trajectory "${t}" --time-duration "$DURATION" --amplitude "${a}" --period "${p}" --ctrl mlp --model "model.joblib")
-              echo "${cmd[@]}" >>"$LOG"
-              "${cmd[@]}"
-              cmd=(python ./apps/scripts/plot_track_traj.py "$output" --joint 5 -d "${output%.*}" -s -x)
-              echo "${cmd[@]}" >>"$LOG"
-              "${cmd[@]}"
-            done
-          done
-        done
-      done
-    done
-  done
-fi
-
-# TRAIN DATA SET C
-if [[ " ${DATASET[*]} " =~ " C " ]]; then
-  output="${OUTPUT_BASE}"/SET-C_joint-5_n-predict-10_scaler-minmax_sin_amp-10_period-1.csv
-  cmd=(pdm run ./apps/track_periodic_traj_mlp.py --train-data "${TRAIN_DATA_SET_C[@]}" --joint 5 --n-predict 10 --n-ctrl-period 1 --scaler minmax --output "${output}" --trajectory sin --time-duration "$DURATION" --amplitude 10 --period 1 --ctrl mlp)
-  "${cmd[@]}"
-  for j in "${JOINT[@]}"; do
-    for k in "${N_PREDICT[@]}"; do
-      for s in "${SCALER[@]}"; do
-        for t in "${TRAJECTORY[@]}"; do
-          for a in "${AMPLITUDE[@]}"; do
-            for p in "${PERIOD[@]}"; do
-              echo "TRAIN SET: C, joint: $j, n_predict: $k, scaler: $s, traj: $t, A:$a, T:$p"
-              output="${OUTPUT_BASE}"/SET-C_joint-"$j"_n-predict-"$k"_scaler-"$s"_"$t"_amp-"$a"_period-"$p".csv
-              cmd=(pdm run ./apps/track_periodic_traj_mlp.py --train-data "${TRAIN_DATA_SET_C[@]}" --joint "${j}" --n-predict "${k}" --n-ctrl-period 1 --scaler "${s}" --output "${output}" --trajectory "${t}" --time-duration "$DURATION" --amplitude "${a}" --period "${p}" --ctrl mlp --model "model.joblib")
-              echo "${cmd[@]}" >>"$LOG"
-              "${cmd[@]}"
-              cmd=(python ./apps/scripts/plot_track_traj.py "$output" --joint 5 -d "${output%.*}" -s -x)
-              echo "${cmd[@]}" >>"$LOG"
-              "${cmd[@]}"
-            done
-          done
-        done
-      done
-    done
-  done
-fi
+do_all A "${TRAIN_DATA_SET_A[@]}"
+do_all B "${TRAIN_DATA_SET_B[@]}"
+do_all C "${TRAIN_DATA_SET_C[@]}"
